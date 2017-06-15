03f458a5037e345282847e52a42ded4e9e9c2b2d
Scrat1980 <ivazin3@gmail.com>
2017-06-15 14:33:17 +0300

    Simple routing done

--- index.php
+++ index.php
@@ -6,3 +6,31 @@
  * Time: 13:37
  */
 
+function getInput( $attribute ) {
+    $result = ( isset( $_GET[$attribute] ) )
+        ? $_GET[$attribute]
+        : null;
+
+    return $result;
+}
+
+$controller = getInput('controller');
+$action = getInput('action');
+
+$allSet = ( ! is_null( $controller ) ) && ( ! is_null( $action ) );
+
+if( ! $allSet ) {
+    $controller = 'SiteController';
+    $action = 'index';
+}
+
+$c = new $controller();
+$c->{$action}();
+
+class SiteController
+{
+    public function index()
+    {
+        echo 11;
+    }
+}
